426f2df36add1846c5f43e1b97f76ace629f7d18
Rhythm-Gupta <rhythm18082@iiitd.ac.in>
2020-01-18 19:58:05 +0530

    mymalloc for small allocation

--- memory.c
+++ memory.c
@@ -11,7 +11,7 @@
 #include <fcntl.h>
 #include <assert.h>
 #include "memory.h"
-
+#include <math.h> 
 #define PAGE_SIZE 4096
 
 static void *alloc_from_ram(size_t size)
@@ -31,20 +31,60 @@ static void free_ram(void *addr, size_t size)
 	munmap(addr, size);
 }
 
+int getSize(int k){
+	int ans = -1;
+	for (int i = 4; i<=12; ++i){
+		if (k<pow(2,i)){
+			ans = i-4;
+			break;
+		}
+	}
+	if (ans!=-1){
+		return ans;
+	}
+	return k;
+}
+
 void myfree(void *ptr)
 {
 	printf("myfree is not implemented\n");
 	abort();
 }
 
+void add_to_list(long long int* ptr, long long int * block){
+
+}
+
+long long int* pop_from_list(long long int* ptr){
+
+}
+
 void *mymalloc(size_t size)
 {
-	test = (int*) alloc_from_ram(4096);
-	*test = 4;
-	long long int addr = (long long int)test;
-	addr = addr%4096;
-	printf("%d\n", *test);
-	printf("%lld\n", addr);
+	int calcSize = getSize(size);
+
+	if (calcSize<9){
+		long long int * ptr = free_list[calcSize];
+		int size = pow(2, calcSize+4);
+		if (ptr==NULL){
+			long long int* page = (long long int *) alloc_from_ram(4096);
+			long long int* endLimit = page + 4096;
+			*page = size;
+			page ++;
+			*page = 4096 - 16;
+			page ++;
+			while(page < endLimit ){
+				add_to_list(ptr, page);
+				page += (size/16);
+			}
+		}
+		return pop_from_list(ptr);
+	}
+
+	else{
+
+	}
+
 	printf("mymalloc is not implemented\n");
 	abort();
 	return NULL;
